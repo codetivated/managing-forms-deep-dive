a79ec8e17e7f23bea91f135918822aa5d77d2433
codetivated <melisa@heying.us>
2025-10-16 14:57:20 -0500

    example using localStorage and viewChild to populate email field on page reload

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
   imports: [FormsModule],
 })
 export class LoginComponent {
+  // using viewChild signal from angular/core
+  private form = viewChild<NgForm>('loginForm');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const savedLoginForm = window.localStorage.getItem('savedLoginForm');
+      if (savedLoginForm) {
+        const parsedSavedLoginForm = JSON.parse(savedLoginForm);
+        const savedEmail = parsedSavedLoginForm.email;
+        setTimeout(() => {
+          this.form()?.controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+
+      const subscription = this.form()
+        ?.valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) => {
+            window.localStorage.setItem(
+              'savedLoginForm',
+              JSON.stringify({ email: value.email })
+            );
+          },
+        });
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+
   onSubmit(formData: NgForm) {
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
@@ -17,5 +56,7 @@ export class LoginComponent {
       return;
     }
     console.log(enteredEmail, enteredPassword);
+
+    formData.reset();
   }
 }
