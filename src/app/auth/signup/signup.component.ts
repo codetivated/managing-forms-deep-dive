e538c8a1d0c46932ab7e11af1a9b0fba76ecdb44
codetivated <melisa@heying.us>
2025-10-17 18:58:11 -0500

    add ReactiveFormsModule to sign up form and connect email and password

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, ElementRef } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  private elementRef = inject(ElementRef);
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onReset() {
+    this.signupForm.reset();
+    this.elementRef.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+}
